e666ff57159a9ecab105ad653fed105905e80514
Muhammad-Bilal <meharbilal24525@gmail.com>
2022-07-18 20:31:25 +0500

    Application Db Context Class Added

--- DataBookShop/Data/Models/ApplicationUser.cs
+++ DataBookShop/Data/Models/ApplicationUser.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataBookShop.Data.Models
 {
-    class ApplicationUser
+    public class ApplicationUser
     {
         public int AuthorId { get; set; }
         public string  AuthorName { get; set; }
